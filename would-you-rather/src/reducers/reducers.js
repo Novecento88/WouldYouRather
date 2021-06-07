c275030442479875d7bf9f899a0b3951a12f2ae1
bafficoluca <bafficoluca@gmail.com>
2021-06-07 17:35:54 +0200

    FEAT: reducers/reducers.js implemented

--- would-you-rather/src/reducers/reducers.js
+++ would-you-rather/src/reducers/reducers.js
@@ -3,8 +3,10 @@ import authedUser from "./authedUser";
 import users from "./users";
 import questions from "./questions";
 
-export default combineReducers({
+const rootReducer = combineReducers({
   authedUser,
   users,
   questions,
 });
+
+export default rootReducer;
