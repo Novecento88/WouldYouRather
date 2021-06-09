b6fbd2b3c94b55965efad455972f34121fb75240
bafficoluca <bafficoluca@gmail.com>
2021-06-09 11:23:48 +0200

    FEAT: @material-ui/core library installed. AppBar implemented.

--- would-you-rather/src/components/App.js
+++ would-you-rather/src/components/App.js
@@ -2,15 +2,48 @@ import React, { Component } from "react";
 import "./App.css";
 import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
+import { AppBar, Tabs, Tab } from "@material-ui/core";
 import Home from "./Home";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedTab: 0,
+    };
+  }
+
   componentDidMount() {
     this.props.dispatch(handleInitialData());
   }
 
+  handleTabChange = (event, value) => {
+    this.setState({
+      selectedTab: value,
+    });
+  };
+
   render() {
-    return <div>{this.props.loading === true ? null : <Home />}</div>;
+    return (
+      <div>
+        <AppBar position="static">
+          <Tabs
+            value={this.state.selectedTab}
+            onChange={this.handleTabChange}
+            aria-label="navigation tabs"
+            centered
+          >
+            <Tab label="HOME" />
+            <Tab label="NEW QUESTION" />
+            <Tab label="LEADER BOARD" />
+            <Tab label="HELLO, USER" />
+            <Tab label="LOGOUT" />
+          </Tabs>
+        </AppBar>
+        <div>{this.props.loading === true ? null : <Home />}</div>
+      </div>
+    );
   }
 }
 
