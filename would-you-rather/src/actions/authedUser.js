12058b1f35e03481c405376234cdbba1c543352c
Luca Baffico <bafficoluca@gmail.com>
2021-09-01 17:28:38 +0200

    minor improvments on authedUser actions and reducers

--- would-you-rather/src/actions/authedUser.js
+++ would-you-rather/src/actions/authedUser.js
@@ -1,10 +1,11 @@
 export const SET_AUTHED_USER = "SET_AUTHED_USER";
 export const LOGOUT = "LOGOUT";
 
-export function setAuthedUser(id) {
+export function setAuthedUser(id, name) {
   return {
     type: SET_AUTHED_USER,
     id,
+    name,
   };
 }
 
--- would-you-rather/src/reducers/authedUser.js
+++ would-you-rather/src/reducers/authedUser.js
@@ -3,7 +3,10 @@ import { SET_AUTHED_USER, LOGOUT } from "../actions/authedUser";
 export default function authedUser(state = null, action) {
   switch (action.type) {
     case SET_AUTHED_USER:
-      return action.id;
+      return {
+        id: action.id,
+        name: action.name,
+      };
     case LOGOUT:
       return null;
     default:
